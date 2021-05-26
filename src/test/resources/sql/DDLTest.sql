577b2de6a86b1039939763adc6cd63680ad06b85
lucas.duval.62@gmail.com <lucas.duval.62@gmail.com>
2021-05-26 17:10:15 +0200

    corrections

--- src/test/resources/sql/DDLTest.sql
+++ src/test/resources/sql/DDLTest.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `bdynh`
+-- Base de données : `bdynhtest`
 --
 CREATE DATABASE IF NOT EXISTS `bdynhtest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `bdynhtest`;
