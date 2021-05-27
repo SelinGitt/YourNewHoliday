cc0b163a765c3fdc1af4097b2c7afb0288109449
Valentin J <vltn.justin@gmail.com>
2021-05-27 17:03:24 +0200

    Creation du dao et son implementation

---
+++ src/main/java/persistance/utilisateur/dao/impl/UtilisateurDao.java
@@ -0,0 +1,27 @@
+package persistance.utilisateur.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import persistance.commun.dao.impl.AbstractGenericDao;
+import persistance.utilisateur.dao.IUtilisateurDao;
+import persistance.utilisateur.entity.UtilisateurDo;
+
+/**
+ * Classe UtilisateurDo
+ *
+ * @author Valentin
+ */
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
+public class UtilisateurDao extends AbstractGenericDao<UtilisateurDo> implements IUtilisateurDao {
+
+    /**
+     * Constructeur par défaut
+     */
+    public UtilisateurDao() {
+        // on utilise le constructeur de la superclass avec ProduitDo.class pour récupérer la classe de l'entité.
+        super(UtilisateurDo.class);
+    }
+}
