53f14b58ad061ae882cb469f3e516b8d6f5f950d
thomasbarre07@gmail.com <thomasbarre07@gmail.com>
2021-05-27 11:54:08 +0200

    creation utilisateurDto

---
+++ src/main/java/presentation/utilisateur/UtilisateurDto.java
@@ -0,0 +1,147 @@
+/**
+ * 
+ */
+package presentation.utilisateur;
+
+/**
+ * Classe représentant un utilisateur pour la vue
+ *
+ * @author Meliodas-sama
+ */
+public class UtilisateurDto {
+
+    private String email;
+    private String password;
+    private String reference;
+    private String nom;
+    private String prenom;
+    private String dateInscription;
+    private String estActif;
+
+    /**
+     * Getter for email
+     *
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Setter for email
+     *
+     * @param email the email to set
+     */
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    /**
+     * Getter for password
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Setter for password
+     *
+     * @param password the password to set
+     */
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    /**
+     * Getter for reference
+     *
+     * @return the reference
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     * Setter for reference
+     *
+     * @param reference the reference to set
+     */
+    public void setReference(final String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * Getter for nom
+     *
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Setter for nom
+     *
+     * @param nom the nom to set
+     */
+    public void setNom(final String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Getter for prenom
+     *
+     * @return the prenom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * Setter for prenom
+     *
+     * @param prenom the prenom to set
+     */
+    public void setPrenom(final String prenom) {
+        this.prenom = prenom;
+    }
+
+    /**
+     * Getter for dateInscription
+     *
+     * @return the dateInscription
+     */
+    public String getDateInscription() {
+        return dateInscription;
+    }
+
+    /**
+     * Setter for dateInscription
+     *
+     * @param dateInscription the dateInscription to set
+     */
+    public void setDateInscription(final String dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+
+    /**
+     * Getter for estActif
+     *
+     * @return the estActif
+     */
+    public String getEstActif() {
+        return estActif;
+    }
+
+    /**
+     * Setter for estActif
+     *
+     * @param estActif the estActif to set
+     */
+    public void setEstActif(final String estActif) {
+        this.estActif = estActif;
+    }
+
+}
