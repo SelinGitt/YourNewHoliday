6fd1a9dd50af2ea71c8948a627a9913e539168fd
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-05-27 17:15:02 +0200

    ajout des JUnit

---
+++ src/test/java/persistance/commande/dao/impl/CommandeDaoTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package persistance.commande.dao.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Classe représentant XX
+ *
+ * @author Administrateur
+ *
+ */
+class CommandeDaoTest {
+
+    /**
+     * Test method for {@link persistance.commande.dao.impl.CommandeDao#findByRef(java.lang.String)}.
+     */
+    @Test
+    void testFindByRef() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link persistance.commande.dao.impl.CommandeDao#findByUserId(java.lang.Integer)}.
+     */
+    @Test
+    void testFindByUserId() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link persistance.commun.dao.impl.AbstractGenericDao#findAll()}.
+     */
+    @Test
+    void testFindAll() {
+        fail("Not yet implemented");
+    }
+
+}
