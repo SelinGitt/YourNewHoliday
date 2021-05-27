0d8e61e9149712491aa8f75fb0dd1149994c5da5
Valentin J <vltn.justin@gmail.com>
2021-05-27 09:45:54 +0200

    Ajout d'une mÃéthode pour creer un utilisateur admin, mise a jour avec le nom de role

--- src/main/java/presentation/controller/temp/UserConnecteController.java
+++ src/main/java/presentation/controller/temp/UserConnecteController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import presentation.utilisateur.dto.UtilisateurConnecteDto;
 
 /**
- * Controller temporaire pour mettre des données utilisateur en session
+ * Controller temporaire pour mettre des données utilisateur en session <br>
+ * A supprimer dès que la page connexion est dispo !
  *
  * @author Valentin
  */
@@ -28,14 +29,26 @@ public class UserConnecteController {
     }
 
     /**
-     * Permet de mettre en session un utilisateur pour vos test
+     * Permet de mettre en session un utilisateur client pour vos test
      *
      * @param  session Session actuelle
      * @return         Nom de la jsp a charger
      */
-    @GetMapping("/create")
-    public String mettreUserEnSession(final HttpSession session) {
-        session.setAttribute("utilisateur", this.creerUtilisateur());
+    @GetMapping("/create_client")
+    public String mettreUserClientEnSession(final HttpSession session) {
+        session.setAttribute("utilisateur", this.creerUtilisateurClient());
+        return "user_session";
+    }
+
+    /**
+     * Permet de mettre en session un utilisateur administrateur pour vos test
+     *
+     * @param  session Session actuelle
+     * @return         Nom de la jsp a charger
+     */
+    @GetMapping("/create_admin")
+    public String mettreUserAdminEnSession(final HttpSession session) {
+        session.setAttribute("utilisateur", this.creerUtilisateurAdmin());
         return "user_session";
     }
 
@@ -47,23 +60,42 @@ public class UserConnecteController {
      */
     @GetMapping("/supprimer")
     public String supprimerUser(final HttpSession session) {
-        session.invalidate();
+        session.removeAttribute("utilisateur");
         return "user_session";
     }
 
     /**
-     * Methode pour créer un {@link presentation.utilisateur.dto.UtilisateurConnecteDto}
+     * Methode pour créer un {@link presentation.utilisateur.dto.UtilisateurConnecteDto} Client
      *
      * @return UtilisateurConnecteDto créer
      */
-    private UtilisateurConnecteDto creerUtilisateur() {
+    private UtilisateurConnecteDto creerUtilisateurClient() {
         final var utilisateurConnecteDto = new UtilisateurConnecteDto();
 
         utilisateurConnecteDto.setIdRole("1");
+        utilisateurConnecteDto.setNomRole("Client");
         utilisateurConnecteDto.setIdUtilisateur("25");
         utilisateurConnecteDto.setNbProduitPanier("3");
         utilisateurConnecteDto.setNom("Dupont");
         utilisateurConnecteDto.setPrenom("Marc");
+
+        return utilisateurConnecteDto;
+    }
+
+    /**
+     * Methode pour créer un {@link presentation.utilisateur.dto.UtilisateurConnecteDto} Administrateur
+     *
+     * @return UtilisateurConnecteDto créer
+     */
+    private UtilisateurConnecteDto creerUtilisateurAdmin() {
+        final var utilisateurConnecteDto = new UtilisateurConnecteDto();
+
+        utilisateurConnecteDto.setIdRole("3");
+        utilisateurConnecteDto.setNomRole("Administrateur");
+        utilisateurConnecteDto.setIdUtilisateur("74");
+        utilisateurConnecteDto.setNbProduitPanier("0");
+        utilisateurConnecteDto.setNom("Bartide");
+        utilisateurConnecteDto.setPrenom("Eric");
 
         return utilisateurConnecteDto;
     }
