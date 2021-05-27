0f48d3bd136e48fe893cb7ab77f103cd29a641b0
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-05-27 12:46:01 +0200

    Ajout du test unitaire pour le findAll

---
+++ src/main/java/persistance/produit/dao/IProduitDao.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package persistance.produit.dao;
+
+import persistance.commun.dao.IGenericDao;
+import persistance.produit.entity.ProduitDo;
+
+/**
+ * Interface pour le CRUD des produits et findAll pour l'instant
+ *
+ * @author Ilaitsivery Jacques MADIOMANANAO
+ */
+public interface IProduitDao extends IGenericDao<ProduitDo> {
+    // Vide pour le moment
+}
---
+++ src/main/java/persistance/produit/dao/impl/ProduitDao.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package persistance.produit.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import persistance.commun.dao.impl.GenericDao;
+import persistance.produit.dao.IProduitDao;
+import persistance.produit.entity.ProduitDo;
+
+/**
+ * Classe représentant l'implémentation de IProduitDao
+ *
+ * @author Administrateur
+ */
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
+public class ProduitDao extends GenericDao<ProduitDo> implements IProduitDao {
+
+    /**
+     * Constructeur par défaut
+     */
+    public ProduitDao() {
+        // on utilise le constructeur de la superclass avec ProduitDo.class pour récupérer la classe de l'entité.
+        super(ProduitDo.class);
+    }
+
+}
