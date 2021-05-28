86fb68f0b322bdc7679bcfb10f4edd2a1f9fd4be
Valentin <vltn.justin@gmail.com>
2021-05-28 10:57:55 +0200

    Creation du controller

---
+++ src/main/java/presentation/utilisateur/controller/ListerUtilisateurController.java
@@ -0,0 +1,36 @@
+package presentation.utilisateur.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import service.utilisateur.IUtilisateurService;
+
+/**
+ * Controller pour liste les utilisateurs
+ *
+ * @author Valentin
+ */
+@Controller
+@RequestMapping("/lister-utilisateur.do")
+public class ListerUtilisateurController {
+
+    @Autowired
+    private IUtilisateurService iUtilisateurService;
+
+    /**
+     * Permet de récuperer la liste des utilisateurs et d'afficher la jsp
+     *
+     * @param  request HttpServletRequest
+     * @return         Nom de la jsp a charger
+     */
+    @GetMapping
+    public String listerUtilisateurs(final HttpServletRequest request) {
+        request.setAttribute("listeUtilisateur", this.iUtilisateurService.findAllUtilisateurs());
+
+        return "listerUtilisateur";
+    }
+}
