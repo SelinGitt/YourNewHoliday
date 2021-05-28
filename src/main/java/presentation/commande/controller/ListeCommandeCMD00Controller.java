41f3af667b812a889a3f37e370a01a2803730240
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-05-28 14:30:54 +0200

    ajout du controlleur pour afficher l'écran CMD_00

---
+++ src/main/java/presentation/commande/controller/ListeCommandeCMD00Controller.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package presentation.commande.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import presentation.commande.dto.CommandeDto;
+import presentation.utilisateur.dto.UtilisateurConnecteDto;
+import service.commande.ICommandeService;
+
+/**
+ * Classe représentant le controller pour l'écran CMD_00
+ *
+ * @author Ilaitsivery Jacques MADIOMANANA
+ */
+@Controller
+@RequestMapping("/listerCommande.do")
+public class ListeCommandeCMD00Controller {
+
+    @Autowired
+    private ICommandeService iCommandeService;
+
+    /**
+     * Permet d'accéder à la page listerCommande do et de l'hydrater
+     *
+     * @param  session contient la session de l'utilisateur
+     * @return         ModelAndView le modèle qui sera utiliser par la vue
+     */
+    @GetMapping
+    public ModelAndView listerCommande(final HttpSession session) {
+        final var modelAndView = new ModelAndView();
+        final UtilisateurConnecteDto utilisateurConnecte = (UtilisateurConnecteDto) session.getAttribute("utilisateur");
+        final List<CommandeDto> listCommande = this.iCommandeService
+                .listerCommandesUtilisateur(Integer.valueOf(utilisateurConnecte.getIdUtilisateur()));
+        modelAndView.setViewName("listerCommande");
+        modelAndView.getModelMap().addAttribute("listCommande", listCommande);
+        return modelAndView;
+    }
+
+}
