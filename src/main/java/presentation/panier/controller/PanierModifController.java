5797351b57d96fb76ccc00e7c5e62fff84057a0c
nathan.brichet <nathan.brichet@live.fr>
2021-06-03 17:06:45 +0200

    ajout de la méthode modifier dans le controller

--- src/main/java/presentation/panier/controller/PanierModifController.java
+++ src/main/java/presentation/panier/controller/PanierModifController.java
@@ -3,8 +3,17 @@
  */
 package presentation.panier.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import presentation.panier.dto.PanierDto;
+import service.panier.IPanierService;
 
 /**
  * Classe représentant le controller pour modifier le panier
@@ -15,4 +24,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "panier.do")
 public class PanierModifController {
 
+    @Autowired
+    private IPanierService iPanierService;
+
+    @GetMapping
+    public ModelAndView modifier(final HttpSession session, final HttpServletRequest request) {
+        final var modelAndView = new ModelAndView();
+        modelAndView.setViewName("panier");
+        final PanierDto panier = (PanierDto) session.getAttribute("panier");
+        final String idProduit = request.getParameter("idProduit");
+        final String quantite = request.getParameter("quantite");
+        iPanierService.updatePanier(panier, Integer.valueOf(idProduit), Integer.valueOf(quantite));
+        return modelAndView;
+    }
 }
