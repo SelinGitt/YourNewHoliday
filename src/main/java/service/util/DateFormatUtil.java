61f848f518b44bf3df58f402f8bc7c81684a984e
ledieu.pierree@gmail.com <ledieu.pierree@gmail.com>
2021-06-03 13:58:43 +0200

    ajout de la methode pour StringToDate

--- src/main/java/service/util/DateFormatUtil.java
+++ src/main/java/service/util/DateFormatUtil.java
@@ -1,8 +1,12 @@
 package service.util;
 
 import java.text.Format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Classe unitaire pour formater une date
@@ -10,6 +14,14 @@ import java.util.Date;
  * @author Pierre
  */
 public class DateFormatUtil {
+	private static final Logger logger = LoggerFactory.getLogger(DateFormatUtil.class);
+
+	/**
+	 * Constructeur
+	 */
+	private DateFormatUtil() {
+		// empty
+	}
 
 	/**
 	 * Méthode permttant de formater une date dans la forme dd/MM/yyyy
@@ -17,9 +29,24 @@ public class DateFormatUtil {
 	 * @param  date la date que l'on formater
 	 * @return      un strig de la date formater
 	 */
-	public static String formaterDate(final Date date) {
+	public static String formaterDateToString(final Date date) {
 		final Format formatter = new SimpleDateFormat("dd/MM/yyyy");
 		final String dateFormat = formatter.format(date);
 		return dateFormat;
+	}
+
+	/**
+	 * Permet de mapper un string de la forne dd//MM/yy en date
+	 * 
+	 * @param  date le string a passer en date
+	 * @return      le date en objet date
+	 */
+	public static Date formaterStringToDate(final String date) {
+		try {
+			return new SimpleDateFormat("dd/MM/yyyy").parse(date);
+		} catch (final ParseException exception) {
+			logger.warn(exception.getMessage());
+		}
+		return new Date();
 	}
 }
--- src/test/java/service/util/DateFormatUtilTest.java
+++ src/test/java/service/util/DateFormatUtilTest.java
@@ -11,18 +11,33 @@ import org.junit.jupiter.api.Test;
 class DateFormatUtilTest {
 
 	/**
-	 * Test method for {@link service.util.DateFormatUtil#formaterDate()}.
+	 * Test method for {@link service.util.DateFormatUtil#formaterDateToString()}.
 	 */
 	@Test
-	void testFormaterDate() {
+	void testFormaterDateToString() {
 		final Calendar calendar = Calendar.getInstance();
 		calendar.set(2021, 5, 3);
 		final Date date = calendar.getTime();
-		final String dateFormate = DateFormatUtil.formaterDate(date);
+		final String dateFormate = DateFormatUtil.formaterDateToString(date);
 		assertNotNull(date);
 		assertNotNull(dateFormate);
 		assertEquals("03/06/2021", dateFormate);
 
+	}
+
+	/**
+	 * Test method for {@link service.util.DateFormatUtil#formaterDateToString()}.
+	 */
+	@Test
+	void testFormaterStringToDate() {
+		final String date = "03/06/2021";
+		final Date dateClasse = DateFormatUtil.formaterStringToDate(date);
+		final Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dateClasse);
+		assertNotNull(dateClasse);
+		assertEquals(3, calendar.get(Calendar.DAY_OF_MONTH));
+		assertEquals(5, calendar.get(Calendar.MONTH));
+		assertEquals(2021, calendar.get(Calendar.YEAR));
 	}
 
 }
