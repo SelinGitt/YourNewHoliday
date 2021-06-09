d6d8a1f83f7c89459ef8bdc2f108c02bca7d0a75
lucas.duval.62@gmail.com <lucas.duval.62@gmail.com>
2021-06-09 13:53:11 +0200

    correction jupiter

--- src/test/java/persistance/produit/dao/impl/ProduitDaoTest.java
+++ src/test/java/persistance/produit/dao/impl/ProduitDaoTest.java
@@ -3,9 +3,9 @@
  */
 package persistance.produit.dao.impl;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
