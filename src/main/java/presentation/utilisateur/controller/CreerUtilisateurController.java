5085c096f8503cc3ae8360aaa8038dc404fe5328
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2021-06-09 12:03:06 +0200

    MAJ controller

--- src/main/java/presentation/utilisateur/controller/CreerUtilisateurController.java
+++ src/main/java/presentation/utilisateur/controller/CreerUtilisateurController.java
@@ -4,7 +4,9 @@
 package presentation.utilisateur.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +21,11 @@ import presentation.utilisateur.dto.UtilisateurDto;
 @Controller
 @RequestMapping("/creerUtilisateur.do")
 public class CreerUtilisateurController {
+    
+    @InitBinder
+    private void initBinder(final WebDataBinder webDataBinder) {
+        webDataBinder.getBindingResult();
+    }
 
     /**
      * Permet de traiter les requêtes GET<br/>
