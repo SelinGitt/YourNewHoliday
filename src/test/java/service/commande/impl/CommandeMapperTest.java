1a13cc7cb51e43aa7a1af7650cf86a5c5db5b165
ledieu.pierree@gmail.com <ledieu.pierree@gmail.com>
2021-06-09 16:22:49 +0200

    correction faute de frappe + changement dans les tests

--- src/test/java/service/commande/impl/CommandeMapperTest.java
+++ src/test/java/service/commande/impl/CommandeMapperTest.java
@@ -10,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -43,7 +42,7 @@ class CommandeMapperTest {
 		final Date date = DateFormatUtil.formaterStringToDate("01/01/1970");
 		commandeDo.setDate(date);
 		commandeDo.setPrixTotal(new BigDecimal(200.40).setScale(2, RoundingMode.FLOOR));
-		commandeDo.setQuaniteTotale(5);
+		commandeDo.setQuantiteTotale(5);
 
 		final CommandeDto commandeDto = CommandeMapper.mapperToDto(commandeDo);
 
@@ -69,16 +68,18 @@ class CommandeMapperTest {
 		final CommandeDo commandeDo = new CommandeDo();
 		commandeDo.setId(20);
 		commandeDo.setReference("ABC9");
-		final Date date = new SimpleDateFormat("dd/MM/yyyy").parse("12/12/1990");
+		final Date date = DateFormatUtil.formaterStringToDate("12/12/1990");
 		commandeDo.setDate(date);
 		commandeDo.setPrixTotal(new BigDecimal(2785.40).setScale(2, RoundingMode.FLOOR));
+		commandeDo.setQuantiteTotale(2);
 
 		final CommandeDo commandeDo2 = new CommandeDo();
 		commandeDo2.setId(23);
 		commandeDo2.setReference("EFG4");
-		final Date date2 = new SimpleDateFormat("dd/MM/yyyy").parse("13/06/1990");
+		final Date date2 = DateFormatUtil.formaterStringToDate("13/06/1990");
 		commandeDo2.setDate(date2);
 		commandeDo2.setPrixTotal(new BigDecimal(2785.40).setScale(2, RoundingMode.FLOOR));
+		commandeDo2.setQuantiteTotale(3);
 
 		assertEquals(2, CommandeMapper.mapperListDoToDto(Arrays.asList(commandeDo, commandeDo2)).size());
 
