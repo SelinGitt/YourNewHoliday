c28dbaed8a47964527e517ea971faff61bc665af
lucas.duval.62@gmail.com <lucas.duval.62@gmail.com>
2021-06-09 13:34:59 +0200

    création de findById et création d'une méthode de test

--- src/main/java/persistance/commun/dao/IGenericDao.java
+++ src/main/java/persistance/commun/dao/IGenericDao.java
@@ -20,5 +20,12 @@ public interface IGenericDao<T> {
      */
     List<T> findAll();
 
+    /**
+     * Permet de trouver une entité T en fonction de son ID en base
+     *
+     * @param  id l'id de l'entité T à trouver
+     * @return    l'entité T trouvée
+     */
+    T findById(final Integer id);
     // Les autres méthodes de CRUD de l'interface
 }
