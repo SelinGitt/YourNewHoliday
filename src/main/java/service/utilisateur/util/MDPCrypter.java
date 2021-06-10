3124bf87615c85767e6c4fb593bf280822067919
thomasbarre07@gmail.com <thomasbarre07@gmail.com>
2021-06-10 15:09:09 +0200

    chiffrement password sonar fix 6

--- src/main/java/service/utilisateur/util/MDPCrypter.java
+++ src/main/java/service/utilisateur/util/MDPCrypter.java
@@ -42,7 +42,7 @@ public class MDPCrypter {
             //ajouter de la complexité au password
             final char[] hexDigit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-            for (int j = 0; j < output.length; j++) {
+            for (var j = 0; j < output.length; j++) {
                 buf.append(hexDigit[(output[j] >> 4) & 0x0f]);
             }
 
