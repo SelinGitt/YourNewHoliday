6db9717f8ac096fda5a38ec2a076113e4ae925b0
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2021-06-11 11:04:06 +0200

    add construteur/getters/setters

--- src/main/java/persistance/utilisateur/entity/PossedeDo.java
+++ src/main/java/persistance/utilisateur/entity/PossedeDo.java
@@ -29,4 +29,65 @@ public class PossedeDo {
     @ManyToOne
     @JoinColumn(name = "idDroit")
     private DroitDo droit;
+
+    /**
+     * Constructor
+     */
+    public PossedeDo() {
+        // empty
+    }
+
+    /**
+     * Getter for id
+     *
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Setter for id
+     *
+     * @param id the id to set
+     */
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter for role
+     *
+     * @return the role
+     */
+    public RoleDo getRole() {
+        return role;
+    }
+
+    /**
+     * Setter for role
+     *
+     * @param role the role to set
+     */
+    public void setRole(final RoleDo role) {
+        this.role = role;
+    }
+
+    /**
+     * Getter for droit
+     *
+     * @return the droit
+     */
+    public DroitDo getDroit() {
+        return droit;
+    }
+
+    /**
+     * Setter for droit
+     *
+     * @param droit the droit to set
+     */
+    public void setDroit(final DroitDo droit) {
+        this.droit = droit;
+    }
 }
