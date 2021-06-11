f196faff4080f3040d4033e0999f401d3c7a9afe
Valentin <vltn.justin@gmail.com>
2021-06-11 13:46:10 +0200

    Ajout de la methode findRole, et tests

--- src/main/java/persistance/utilisateur/dao/IDroitDao.java
+++ src/main/java/persistance/utilisateur/dao/IDroitDao.java
@@ -3,6 +3,8 @@
  */
 package persistance.utilisateur.dao;
 
+import java.util.List;
+
 import persistance.commun.dao.IGenericDao;
 import persistance.utilisateur.entity.DroitDo;
 
@@ -13,4 +15,11 @@ import persistance.utilisateur.entity.DroitDo;
  */
 public interface IDroitDao extends IGenericDao<DroitDo> {
 
+    /**
+     * Permet de trouver les roles lie a un droit
+     *
+     * @param  droit Droit ou il faut trouver les roles
+     * @return       List des libelle des roles
+     */
+    List<String> findRole(final DroitDo droit);
 }
--- src/main/java/persistance/utilisateur/dao/impl/DroitDao.java
+++ src/main/java/persistance/utilisateur/dao/impl/DroitDao.java
@@ -3,6 +3,13 @@
  */
 package persistance.utilisateur.dao.impl;
 
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +27,25 @@ import persistance.utilisateur.entity.DroitDo;
 @Transactional(propagation = Propagation.MANDATORY)
 public class DroitDao extends AbstractGenericDao<DroitDo> implements IDroitDao {
 
+    private static final Logger logger = LoggerFactory.getLogger(DroitDao.class);
+
     /**
      * Constructeur par défaut
      */
     public DroitDao() {
         // on utilise le constructeur de la superclass avec DroitDo.class pour récupérer la classe de l'entité.
         super(DroitDo.class);
+    }
+
+    @Override
+    public List<String> findRole(final DroitDo droit) {
+        final TypedQuery<String> query = entityManager
+                .createQuery("SELECT r.libelle FROM RoleDo r, DroitDo d WHERE d.url LIKE '" + droit.getUrl() + "'", String.class);
+        try {
+            return query.getResultList();
+        } catch (final NoResultException exception) {
+            logger.info("Pas de droits pour {}", droit.getUrl(), exception);
+            return null;
+        }
     }
 }
