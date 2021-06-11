09b06bc874a3e3d401971f9636654fd108c60243
Valentin <vltn.justin@gmail.com>
2021-06-11 14:51:21 +0200

    Ajout des annotations entity et table

--- src/main/java/persistance/utilisateur/entity/PossedeDo.java
+++ src/main/java/persistance/utilisateur/entity/PossedeDo.java
@@ -4,17 +4,21 @@
 package persistance.utilisateur.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * Classe représentant PossedeDo
  *
- * @author Chamalo
+ * @author Valentin
  */
+@Entity
+@Table(name = "possede")
 public class PossedeDo {
 
     @Id
