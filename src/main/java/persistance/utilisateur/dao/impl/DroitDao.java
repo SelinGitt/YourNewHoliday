c2f97cb319fb2418a99d1ac6ab5723072b3c587e
Valentin <vltn.justin@gmail.com>
2021-06-11 13:54:14 +0200

    Fix avec sonnar

--- src/main/java/persistance/utilisateur/dao/impl/DroitDao.java
+++ src/main/java/persistance/utilisateur/dao/impl/DroitDao.java
@@ -5,11 +5,8 @@ package persistance.utilisateur.dao.impl;
 
 import java.util.List;
 
-import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,8 +24,6 @@ import persistance.utilisateur.entity.DroitDo;
 @Transactional(propagation = Propagation.MANDATORY)
 public class DroitDao extends AbstractGenericDao<DroitDo> implements IDroitDao {
 
-    private static final Logger logger = LoggerFactory.getLogger(DroitDao.class);
-
     /**
      * Constructeur par défaut
      */
@@ -39,13 +34,10 @@ public class DroitDao extends AbstractGenericDao<DroitDo> implements IDroitDao {
 
     @Override
     public List<String> findRole(final DroitDo droit) {
-        final TypedQuery<String> query = entityManager
-                .createQuery("SELECT r.libelle FROM RoleDo r, DroitDo d WHERE d.url LIKE '" + droit.getUrl() + "'", String.class);
-        try {
-            return query.getResultList();
-        } catch (final NoResultException exception) {
-            logger.info("Pas de droits pour {}", droit.getUrl(), exception);
-            return null;
-        }
+        final TypedQuery<String> query = entityManager.createQuery("SELECT r.libelle FROM RoleDo r, DroitDo d WHERE d.url LIKE :urlDroit",
+                String.class);
+        query.setParameter("urlDroit", droit.getUrl());
+
+        return query.getResultList();
     }
 }
