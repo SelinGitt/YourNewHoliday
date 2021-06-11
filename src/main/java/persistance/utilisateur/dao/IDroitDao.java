ddedd2e7ebaef0036141b9eb65808ff7c6f34b7b
Valentin <vltn.justin@gmail.com>
2021-06-11 12:08:13 +0200

    Creation du dao pour droit

---
+++ src/main/java/persistance/utilisateur/dao/IDroitDao.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package persistance.utilisateur.dao;
+
+import persistance.commun.dao.IGenericDao;
+import persistance.utilisateur.entity.DroitDo;
+
+/**
+ * Interface IDroitDao
+ *
+ * @author Valentin
+ */
+public interface IDroitDao extends IGenericDao<DroitDo> {
+
+}
