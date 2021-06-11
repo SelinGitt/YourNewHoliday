c1d61a713ec444042b383df9060ebee1011c4f8b
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-06-11 12:30:12 +0200

    trie des commandes dans l'ordre anthechornologique

--- src/main/java/service/commande/impl/CommandeService.java
+++ src/main/java/service/commande/impl/CommandeService.java
@@ -3,6 +3,8 @@
  */
 package service.commande.impl;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import persistance.commande.dao.ICommandeDao;
+import persistance.commande.entity.CommandeDo;
 import presentation.commande.dto.CommandeDto;
 import service.commande.CommandeMapper;
 import service.commande.ICommandeService;
@@ -26,14 +29,35 @@ import service.commande.ICommandeService;
 @Transactional(propagation = Propagation.REQUIRED)
 public class CommandeService implements ICommandeService {
 
-    private final Logger logger = LoggerFactory.getLogger(CommandeService.class);
+    /**
+     * pour inverser l'odre de comparaison des dates
+     */
+    private static final int INVERSE_ORDRE = -1;
+
+    private final Logger     logger        = LoggerFactory.getLogger(CommandeService.class);
     @Autowired
-    private ICommandeDao iCommandeDao;
+    private ICommandeDao     iCommandeDao;
 
     @Override
     public List<CommandeDto> listerCommandesUtilisateur(final Integer idUser) {
         logger.info("la liste  des commandes utilisateur avec  idUser {}", idUser);
-        return CommandeMapper.mapperListDoToDto(iCommandeDao.findByUserId(idUser));
+        final List<CommandeDo> listCommandeDo = this.iCommandeDao.findByUserId(idUser);
+        this.trierCommande(listCommandeDo);
+        return CommandeMapper.mapperListDoToDto(listCommandeDo);
+    }
+
+    /**
+     * Permet de trier une liste de CommandeDo à partir des dates dans l'ordre antéchronologie
+     *
+     * @param listCommandeDo la liste à trier
+     */
+    private void trierCommande(final List<CommandeDo> listCommandeDo) {
+        Collections.sort(listCommandeDo, new Comparator<CommandeDo>() {
+            @Override
+            public int compare(final CommandeDo commande1, final CommandeDo commande2) {
+                return INVERSE_ORDRE * commande1.getDate().compareTo(commande2.getDate());
+            }
+        });
     }
 
 }
