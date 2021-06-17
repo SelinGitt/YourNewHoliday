197406e5ad820e930d595c4e354a8b5ebb31ddac
damien.dugaz@gmail.com <damien.dugaz@gmail.com>
2021-06-17 14:34:29 +0200

    hashage mot de passe passage en SHA512 - sonar

--- src/main/java/service/utilisateur/util/MDPCrypter.java
+++ src/main/java/service/utilisateur/util/MDPCrypter.java
@@ -40,7 +40,7 @@ public class MDPCrypter {
             byte[] output = md.digest();
 
             //ajouter de la complexité au password
-            final char[] hexDigit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            final var hexDigit = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
             for (var j = 0; j < output.length; j++) {
                 buf.append(hexDigit[(output[j] >> 4) & 0x0f]);
