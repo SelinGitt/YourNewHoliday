574f8f3b015c2be50fed85328b5c07535d2d4800
damien.dugaz@gmail.com <damien.dugaz@gmail.com>
2021-06-17 15:55:31 +0200

    usr07 validation changement regex

--- src/main/java/presentation/utilisateur/validator/ConnecterValidator.java
+++ src/main/java/presentation/utilisateur/validator/ConnecterValidator.java
@@ -3,8 +3,6 @@
  */
 package presentation.utilisateur.validator;
 
-import java.util.regex.Pattern;
-
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -32,10 +30,9 @@ public class ConnecterValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "usr07.erreur.password_vide", "Default Error");
         final var utilisateurDto = (UtilisateurDto) target;
         //        Vérification du format de l'adresse email
-        final var password = utilisateurDto.getEmail();
-        if (!password.isBlank() && !password.matches(Pattern.quote("^\\S+@\\S+$"))) {
+        final var password = utilisateurDto.getPassword();
+        if (!password.isBlank() && !password.matches(("^\\S+@\\S+$"))) {
             errors.rejectValue("email", "usr07.erreur.email_format", new Object[] {password}, "Default Error");
-
         }
     }
 }
