e117776d5052a7b5ee5c40a42a3f42c4907d8b5c
nathan.brichet <nathan.brichet@live.fr>
2021-06-18 15:02:42 +0200

    correction sonar 1

--- src/main/java/presentation/startup/StartUpApp.java
+++ src/main/java/presentation/startup/StartUpApp.java
@@ -5,6 +5,8 @@ package presentation.startup;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -21,14 +23,16 @@ import service.util.GetPropertyValues;
 public class StartUpApp implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
-    private GetPropertyValues getPropertyValues;
+    private GetPropertyValues   getPropertyValues;
+
+    private static final Logger logger = LoggerFactory.getLogger(StartUpApp.class);
 
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
         try {
             getPropertyValues.getPropValues();
         } catch (final IOException exception) {
-            exception.printStackTrace();
+            logger.error("Le chargement du fichier YNH-application.properties a généré une erreur : ", exception);
         }
     }
 
--- src/main/java/service/util/GetPropertyValues.java
+++ src/main/java/service/util/GetPropertyValues.java
@@ -5,11 +5,12 @@ package service.util;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,6 +20,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GetPropertyValues {
+
+    private static final Logger             logger        = LoggerFactory.getLogger(GetPropertyValues.class);
 
     /**
      * Map contennant les chemins d'accès aux répertoires
@@ -31,9 +34,9 @@ public class GetPropertyValues {
      * @throws IOException
      */
     public void getPropValues() throws IOException {
-        final Properties prop = new Properties();
-        final String propFileName = "YNH-application.properties";
-        try (final InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName)) {
+        final var prop = new Properties();
+        final var propFileName = "YNH-application.properties";
+        try (final var inputStream = getClass().getClassLoader().getResourceAsStream(propFileName)) {
 
             if (inputStream != null) {
                 prop.load(inputStream);
@@ -41,11 +44,8 @@ public class GetPropertyValues {
                 throw new FileNotFoundException("The property file " + propFileName + " not found in the classpath.");
             }
             PROPERTIESMAP.put("contactRepo", prop.getProperty("contactRepo"));
-        } catch (
-
-        final Exception e) {
-
-            System.out.println("Exception: " + e);
+        } catch (final Exception e) {
+            logger.error("Le stream du fichier YNH-application.properties a généré une exception : ", e);
         }
     }
 }
