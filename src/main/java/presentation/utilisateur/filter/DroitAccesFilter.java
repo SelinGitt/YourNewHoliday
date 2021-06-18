dbda8da52800d2af28100e125c7ed920140ce0b4
Valentin <vltn.justin@gmail.com>
2021-06-18 11:12:58 +0200

    Fix de l'attribute pour rÃecup utilisateur en session

--- src/main/java/presentation/utilisateur/filter/DroitAccesFilter.java
+++ src/main/java/presentation/utilisateur/filter/DroitAccesFilter.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import presentation.startup.StartupApp;
-import presentation.utilisateur.controller.ConnecterController;
 import presentation.utilisateur.dto.UtilisateurConnecteDto;
 
 /**
@@ -48,7 +47,7 @@ public class DroitAccesFilter implements Filter {
             return;
         }
 
-        final var user = (UtilisateurConnecteDto) request.getSession().getAttribute(ConnecterController.UTILISATEUR);
+        final var user = (UtilisateurConnecteDto) request.getSession().getAttribute("utilisateur");
 
         // Si user null = visiteur; sinon check rang
         if (user == null) {
