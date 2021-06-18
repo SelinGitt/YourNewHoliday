0a7ae8fe9171fea552ddf4ffb6a30b0448b11ede
Valentin <vltn.justin@gmail.com>
2021-06-18 10:24:55 +0200

    Ajout de la methode post

--- src/main/java/presentation/utilisateur/controller/ListerUtilisateurController.java
+++ src/main/java/presentation/utilisateur/controller/ListerUtilisateurController.java
@@ -1,11 +1,16 @@
 package presentation.utilisateur.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import presentation.utilisateur.dto.UtilisateurDto;
 import service.utilisateur.IUtilisateurService;
 
 /**
@@ -31,5 +36,28 @@ public class ListerUtilisateurController {
         modelAndView.setViewName("listerUtilisateur");
         modelAndView.getModelMap().addAttribute("listeUtilisateur", this.iUtilisateurService.findAllUtilisateurs());
         return modelAndView;
+    }
+
+    /**
+     * Permet d'effectuee une recherche
+     * 
+     * @param  searchType  Le type de recherche, filtre, ou recherche avec input
+     * @param  searchInput La valeur de la recherche
+     * @return             ModelAndView avec le nom de la jsp et la liste des utilisateurs trie en attribut
+     */
+    @PostMapping
+    public ModelAndView recherche(final @RequestParam(value = "searchType") String searchType,
+            final @RequestParam(value = "searchInput") String searchInput) {
+        final var modelAndView = new ModelAndView("listerUtilisateur");
+
+        if ("search".equals(searchType)) {
+            modelAndView.getModelMap().addAttribute("listeUtilisateur", this.rechercheInput(searchInput));
+        }
+
+        return modelAndView;
+    }
+
+    private List<UtilisateurDto> rechercheInput(final String searchInput) {
+        return null;
     }
 }
