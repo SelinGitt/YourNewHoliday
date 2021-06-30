9f7fbcc57d871c5d004211fbab6c70e9cd2c8fe2
SteveDebuchy <stevedebuchy@gmail.com>
2021-06-30 14:51:52 +0200

    factorisation updatePanier ok

--- src/main/java/service/panier/impl/PanierService.java
+++ src/main/java/service/panier/impl/PanierService.java
@@ -40,44 +40,36 @@ public class PanierService implements IPanierService {
         final Map<ProduitDto, LigneCommandeProduitDto> mapPanier = panier.getMapPanier();
         // On récupère la ligne de commande
         LigneCommandeProduitDto ligneCommande = mapPanier.get(produitAjout);
-        // si le produit n'était pas dans le panier 
-        // le nombre de produits ajoutés à la map correspondra à la quantité en paramètre.
+        int quantiteProduit = 0;
+        // si le produit n'est pas dans le panier 
         if (ligneCommande == null) {
-            // on l'ajoute si la quantité n'est pas nulle ou négative
-            if (quantite > 0) {
-                // on crée la ligne de commande
-                ligneCommande = new LigneCommandeProduitDto();
-                // on ajoute la quantité
-                ligneCommande.setQuantite(quantite);
-                // On récupère le prix unitaire               
-                final var prixUnitaire = Double.valueOf(produitAjout.getPrixUnitaire());
-                // On calcule le prix
-                final Double prix = prixUnitaire * quantite;
-                // on ajoute le prix formaté à la ligne de commande
-                ligneCommande.setPrix(DecimalFormatUtils.decimalFormatUtil(prix));
-                // on mets à jour la map
-                mapPanier.put(produitAjout, ligneCommande);
+            // on effectue la modification que si la quantité n'est pas nulle ou négative
+            if (quantite < 1) {
+                return panier;
             }
-            // sinon, on mets à jour la quantité existante
+            // le nombre de produits ajoutés à la map correspondra à la quantité en paramètre.            
+            quantiteProduit = quantite;
+            // On initialise la ligne de commande
+            ligneCommande = new LigneCommandeProduitDto();
         } else {
-            final Integer quantiteProduit = ligneCommande.getQuantite() + quantite;
-            // si la quantité du produit que l'on met à jour devient nulle ou négative, 
-            // alors le produit est supprimé du panier.
-            if (quantiteProduit < 1) {
-                mapPanier.remove(produitAjout);
-                // on met à jour le produit dans la map du panier avec sa nouvelle quantité.
-            } else {
-                // on modifie la ligne de commande
-                ligneCommande.setQuantite(quantiteProduit);
-                // On récupère le prix unitaire
-                final var prixUnitaire = Double.valueOf(produitAjout.getPrixUnitaire());
-                // On calcule le prix
-                final Double prix = prixUnitaire * quantiteProduit;
-                // on ajoute le prix formaté à la ligne de commande
-                ligneCommande.setPrix(DecimalFormatUtils.decimalFormatUtil(prix));
-                // on mets à jour la map
-                mapPanier.put(produitAjout, ligneCommande);
-            }
+            // sinon, on mets à jour la quantité existante            
+            quantiteProduit = ligneCommande.getQuantite() + quantite;
+        }
+        // si la quantité du produit que l'on met à jour devient nulle ou négative, 
+        // alors le produit est supprimé du panier.
+        if (quantiteProduit < 1) {
+            mapPanier.remove(produitAjout);
+        } else {
+            // on modifie la ligne de commande
+            ligneCommande.setQuantite(quantiteProduit);
+            // On récupère le prix unitaire
+            final var prixUnitaire = Double.valueOf(produitAjout.getPrixUnitaire());
+            // On calcule le prix
+            final Double prix = prixUnitaire * quantiteProduit;
+            // on ajoute le prix formaté à la ligne de commande
+            ligneCommande.setPrix(DecimalFormatUtils.decimalFormatUtil(prix));
+            // on mets à jour la map
+            mapPanier.put(produitAjout, ligneCommande);
         }
         // on met à jour le nombre de référence dans le panier.
         panier.setNombreDeReferences(panier.getMapPanier().size());
