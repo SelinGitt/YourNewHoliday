5432ee36c0d3fca8a9ddec87ad419765ae6d61dc
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-01 15:56:46 +0200

    implementation controller v1.0

--- src/main/java/presentation/panier/controller/ModifierQuantiteController.java
+++ src/main/java/presentation/panier/controller/ModifierQuantiteController.java
@@ -3,8 +3,15 @@
  */
 package presentation.panier.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import presentation.panier.dto.PanierDto;
+import service.panier.IPanierService;
 
 /**
  * Class represents controller pour les boutons modifiant la quantité d'une produit du panier
@@ -15,5 +22,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/modifierQuantite.do")
 public class ModifierQuantiteController {
+    @Autowired
+    private IPanierService panierService;
+
+    @GetMapping
+    public ModelAndView modifierQuantite(final @SessionAttribute("panierDto") PanierDto panierDto) {
+        final var modelAndView = new ModelAndView();
+
+        System.out.println("coucou");
+        //renvoyer vers listerPanierProduitController
+        modelAndView.setViewName("redirect:listerPanierProduits.do");
+        return modelAndView;
+    }
 
 }
