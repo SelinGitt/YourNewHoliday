54be8b8bbb0f4ace4fd3a035716d9f7d1237f902
nathan.brichet <nathan.brichet@live.fr>
2021-07-01 10:00:22 +0200

    correction 1 clean install

--- src/main/java/presentation/panier/controller/SupprimerProduitPanierController.java
+++ src/main/java/presentation/panier/controller/SupprimerProduitPanierController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import presentation.panier.dto.LigneCommandeProduitDto;
 import presentation.panier.dto.PanierDto;
 import presentation.produit.dto.ProduitDto;
 
@@ -35,7 +36,7 @@ public class SupprimerProduitPanierController {
     public String supprimerProduitPanier(final HttpSession session, final HttpServletRequest request) {
         final PanierDto panier = (PanierDto) session.getAttribute("panierDto");
         final ProduitDto produitToDelete = findProduitMap(panier, Integer.valueOf(request.getParameter("id")));
-        final Map<ProduitDto, Integer> mapPanier = panier.getMapPanier();
+        final Map<ProduitDto, LigneCommandeProduitDto> mapPanier = panier.getMapPanier();
         mapPanier.remove(produitToDelete);
         panier.setNombreDeReferences(mapPanier.size());
         if (mapPanier.isEmpty()) {
@@ -52,7 +53,7 @@ public class SupprimerProduitPanierController {
      * @return           : le produit recherché
      */
     public ProduitDto findProduitMap(final PanierDto panier, final Integer idProduit) {
-        final Map<ProduitDto, Integer> mapPanier = panier.getMapPanier();
+        final Map<ProduitDto, LigneCommandeProduitDto> mapPanier = panier.getMapPanier();
         for (final ProduitDto produit : mapPanier.keySet()) {
             if (Integer.valueOf(produit.getIdProduitOriginal()) == idProduit) {
                 return produit;
