148ccb90ce2cdbc13dccbaef3c3c719143f2c2e6
nathan.brichet <nathan.brichet@live.fr>
2021-07-01 09:56:14 +0200

    eclipse ok

--- src/main/java/presentation/panier/controller/SupprimerProduitPanierController.java
+++ src/main/java/presentation/panier/controller/SupprimerProduitPanierController.java
@@ -24,14 +24,33 @@ import presentation.produit.dto.ProduitDto;
 @RequestMapping("/supprimerProduitPanier.do")
 public class SupprimerProduitPanierController {
 
+    /**
+     * Permet de supprimer une ligne du panier
+     *
+     * @param  session : le scope session
+     * @param  request : le scope request
+     * @return         : le nom de la vue vers laquelle on souhaite se diriger
+     */
     @GetMapping
     public String supprimerProduitPanier(final HttpSession session, final HttpServletRequest request) {
         final PanierDto panier = (PanierDto) session.getAttribute("panierDto");
         final ProduitDto produitToDelete = findProduitMap(panier, Integer.valueOf(request.getParameter("id")));
-        panier.getMapPanier().remove(produitToDelete);
-        return "listerPanierProduits";
+        final Map<ProduitDto, Integer> mapPanier = panier.getMapPanier();
+        mapPanier.remove(produitToDelete);
+        panier.setNombreDeReferences(mapPanier.size());
+        if (mapPanier.isEmpty()) {
+            return "pan_00_vide";
+        }
+        return "pan_00";
     }
 
+    /**
+     * Permet de retrouver un produit dans la map à partir de son ID
+     *
+     * @param  panier    : le panier en session
+     * @param  idProduit : l'id du produit recherché
+     * @return           : le produit recherché
+     */
     public ProduitDto findProduitMap(final PanierDto panier, final Integer idProduit) {
         final Map<ProduitDto, Integer> mapPanier = panier.getMapPanier();
         for (final ProduitDto produit : mapPanier.keySet()) {
