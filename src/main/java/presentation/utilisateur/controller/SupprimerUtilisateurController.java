0942bb2bd7d78f9bd46a341f06f010ea16d8bc76
damien.dugaz@gmail.com <damien.dugaz@gmail.com>
2021-07-07 16:22:39 +0200

    correctifs MR rejetée

--- src/main/java/presentation/utilisateur/controller/SupprimerUtilisateurController.java
+++ src/main/java/presentation/utilisateur/controller/SupprimerUtilisateurController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,7 +24,6 @@ import service.utilisateur.IUtilisateurService;
  */
 @Controller
 @RequestMapping("/supprimerUtilisateur.do")
-@SessionAttributes("utilisateur")
 public class SupprimerUtilisateurController {
 
     @Autowired
