7370725bd378c703c34594de77015220243deb04
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-08 10:08:46 +0200

    corrections br

--- src/main/java/service/util/DecimalFormatUtils.java
+++ src/main/java/service/util/DecimalFormatUtils.java
@@ -92,7 +92,7 @@ public class DecimalFormatUtils {
      * Permet de savoir si le prix contient une virgurle
      * 
      * @param  prixAVerifier le String à  vérifier
-     * @return               true si le prix contient une virgule <br/>
+     * @return               true si le prix contient une virgule <br>
      *                       false sinon
      */
     public static boolean isPrixAVirgule(final String prixAVerifier) {
@@ -100,7 +100,7 @@ public class DecimalFormatUtils {
     }
 
     /**
-     * Permet de transformer un nombre contenu dans un string ayant été formaté <br />
+     * Permet de transformer un nombre contenu dans un string ayant été formaté <br>
      * par la méthode formatUtil(Big)Decimal en Double exploitable
      *
      * @param  nombreString : le nombre à  transformer
