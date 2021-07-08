4bef6e27794b026516d8b1be2bc078f9f2f340a2
Valentin J <vltn.justin@gmail.com>
2021-07-08 17:12:42 +0200

    Fichier de test

---
+++ src/test/java/service/utilisateur/util/UtilisateurRoleEnumTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package service.utilisateur.util;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test pour {@link service.utilisateur.util.UtilisateurRoleEnum}
+ *
+ * @author Valentin
+ */
+class UtilisateurRoleEnumTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
