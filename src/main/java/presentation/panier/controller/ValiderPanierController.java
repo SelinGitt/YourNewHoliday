ac8a4c6da6c7ea6afa6864a9a4bd6a78ea6335b3
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-07-08 12:03:37 +0200

    traitement du cas utilisateur KO

---
+++ src/main/java/presentation/panier/controller/ValiderPanierController.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package presentation.panier.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import presentation.panier.dto.PanierDto;
+import presentation.utilisateur.dto.UtilisateurConnecteDto;
+import service.panier.IPanierService;
+import service.utilisateur.IUtilisateurService;
+
+/**
+ * Classe représentant le Controlleur pour Valider le panier
+ *
+ * @author Ilaitsivery Jacques MADIOMANANA
+ */
+@Controller
+@RequestMapping("/validerPanier.do")
+public class ValiderPanierController {
+
+    @Autowired
+    private IUtilisateurService iUtilisateurService;
+
+    @Autowired
+    private IPanierService      panierService;
+
+    /**
+     * Permet de passer un Panier à commande si toutes les conditions sont remplies
+     *
+     * @param  panierDto   le panier de l'utilisateur
+     * @param  utilisateur l'utilisateur connecté
+     * @return             String l'url vers lequel on doit se rendre
+     */
+    @GetMapping
+    public String passerPanierACommande(final @SessionAttribute("panierDto") PanierDto panierDto,
+            final @SessionAttribute("utilisateur") UtilisateurConnecteDto utilisateur) {
+        if (this.iUtilisateurService.findUtilisateurById(Integer.parseInt(utilisateur.getIdUtilisateur())) == null) {
+            // On détruit la session donc le panier sera vider automatiquement
+            return "redirect:deconnecter.do";
+        }
+        return "forward:detailCommande.do?ref=ABC3";
+    }
+}
--- src/test/resources/sql/DML.sql
+++ src/test/resources/sql/DML.sql
@@ -66,6 +66,7 @@ INSERT INTO `droit` (`idDroit`, `url`) VALUES (19, 'supprimerUtilisateur.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (20, 'supprimerProduitPanier.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (21, 'viderPanier.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (22, 'modifierQuantite.do');
+INSERT INTO `droit` (`idDroit`, `url`) VALUES (23, 'validerPanier.do');
 
 --
 -- Déchargement des données de la table `role`
@@ -170,4 +171,6 @@ INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (41, 21, 1);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (42, 21, 3);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (43, 22, 1);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (44, 22, 3);
+INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (45, 23, 1);
+INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (46, 23, 3);
 
