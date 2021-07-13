730de15b87afdafd8e9f86d5180aa69e5f7d5f28
madiomanana.ijacques@gmail.com <madiomanana.ijacques@gmail.com>
2021-07-13 11:47:11 +0200

    changement de remplir panier pour le test (sera effacer par la suite)

--- src/main/java/presentation/temp/RemplirPanier.java
+++ src/main/java/presentation/temp/RemplirPanier.java
@@ -45,15 +45,6 @@ public class RemplirPanier {
         produitDto1.setHebergement("Maison dHotes");
         produitDto1.setCheminImage("maldives.jpg");
         produitDto1.setMiseEnVente("true");
-        // ProduitDto2
-        //        final var produitDto2 = new ProduitDto();
-        //        produitDto2.setIdProduitOriginal("2");
-        //        produitDto2.setVersion("1");
-        //        produitDto2.setDescription("Description très courte du voyage sur deux ou trois lignes maximum");
-        //        produitDto2.setPrixUnitaire(DecimalFormatUtils.decimalFormatUtil(500.00));
-        //        produitDto2.setNom("Voyage aux Etats Unis d'Amérique");
-        //        produitDto2.setReference("CCC1865521");
-        //        produitDto2.setCheminImage(null);
         // ProduitDto3
         final var produitDto3 = new ProduitDto();
         produitDto3.setIdProduitOriginal("3");
@@ -68,16 +59,6 @@ public class RemplirPanier {
         produitDto3.setCheminImage("espagne.jpg");
         produitDto3.setHebergement("chambre dhôtel");
         produitDto3.setMiseEnVente("true");
-        // ProduitDto4
-        //        final var produitDto4 = new ProduitDto();
-        //        produitDto4.setIdProduitOriginal("4");
-        //        produitDto4.setVersion("2");
-        //        produitDto4.setDescription(
-        //                "Description courte du voyage sur deux ou trois lignes maximum, un peu de texte en plus pour tester l'affichage");
-        //        produitDto4.setPrixUnitaire(DecimalFormatUtils.decimalFormatUtil(1.90));
-        //        produitDto4.setNom("Voyage au coin de la rue");
-        //        produitDto4.setReference("ZZZ9696969");
-        //        produitDto4.setCheminImage(null);
         // ProduitDto5
         final var produitDto5 = new ProduitDto();
         produitDto5.setIdProduitOriginal("5");
@@ -92,65 +73,25 @@ public class RemplirPanier {
         produitDto5.setCheminImage("tokyo.jpg");
         produitDto5.setHebergement("Appartement");
         produitDto5.setMiseEnVente("true");
-        // ProduitDto6
-        //        final var produitDto6 = new ProduitDto();
-        //        produitDto6.setIdProduitOriginal("6");
-        //        produitDto6.setVersion("1");
-        //        produitDto6.setDescription(
-        //                "Description courte du voyage sur deux ou trois lignes maximum, un peu de texte en plus pour tester l'affichage");
-        //        produitDto6.setPrixUnitaire(DecimalFormatUtils.decimalFormatUtil(1500.00));
-        //        produitDto6.setNom("Voyage voyage et jamais ne reviens");
-        //        produitDto6.setReference("AAA6666666");
-        //        produitDto6.setCheminImage(null);
-
-        // Produit avec équivalence en base 
-        //        final var produitDto7 = new ProduitDto();
-        //        produitDto7.setIdProduitOriginal("7");
-        //        produitDto7.setVersion("0");
-        //        produitDto7.setDescription("La méditérannée, le colysée, les pâtes !");
-        //        produitDto7.setPrixUnitaire(DecimalFormatUtils.decimalFormatUtil(300.00));
-        //        produitDto7.setNom("Voyage en Italie");
-        //        produitDto7.setReference("ITA1289967");
-        //        produitDto7.setCheminImage(null);
 
         // ajout des lignes de commande
         final var ligneCommandeProduit = new LigneCommandeProduitDto();
         ligneCommandeProduit.setQuantite(6);
         ligneCommandeProduit.setPrix(DecimalFormatUtils.decimalFormatUtil(6 * 200.30, Locale.FRANCE));
-        final var ligneCommandeProduit2 = new LigneCommandeProduitDto();
-        ligneCommandeProduit2.setQuantite(8);
-        ligneCommandeProduit2.setPrix(DecimalFormatUtils.decimalFormatUtil(8 * 700.00, Locale.FRANCE));
         final var ligneCommandeProduit3 = new LigneCommandeProduitDto();
         ligneCommandeProduit3.setQuantite(1);
         ligneCommandeProduit3.setPrix(DecimalFormatUtils.decimalFormatUtil(1 * 500.00, Locale.FRANCE));
-        final var ligneCommandeProduit4 = new LigneCommandeProduitDto();
-        ligneCommandeProduit4.setQuantite(3);
-        ligneCommandeProduit4.setPrix(DecimalFormatUtils.decimalFormatUtil(3 * 1.90, Locale.FRANCE));
         final var ligneCommandeProduit5 = new LigneCommandeProduitDto();
         ligneCommandeProduit5.setQuantite(2);
         ligneCommandeProduit5.setPrix(DecimalFormatUtils.decimalFormatUtil(2 * 999.00, Locale.FRANCE));
-        final var ligneCommandeProduit6 = new LigneCommandeProduitDto();
-        ligneCommandeProduit6.setQuantite(3);
-        ligneCommandeProduit6.setPrix(DecimalFormatUtils.decimalFormatUtil(3 * 1500.00, Locale.FRANCE));
-        final var ligneCommandeProduit7 = new LigneCommandeProduitDto();
-        ligneCommandeProduit7.setQuantite(3);
-        ligneCommandeProduit7.setPrix(DecimalFormatUtils.decimalFormatUtil(3 * 300.00, Locale.FRANCE));
 
         // add products to PanierDto
         panierDto.getMapPanier().put(produitDto1, ligneCommandeProduit);
         panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
-        //        panierDto.getMapPanier().put(produitDto2, ligneCommandeProduit2);
-        //        panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
         panierDto.getMapPanier().put(produitDto3, ligneCommandeProduit3);
         panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
-        //        panierDto.getMapPanier().put(produitDto4, ligneCommandeProduit4);
-        //        panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
         panierDto.getMapPanier().put(produitDto5, ligneCommandeProduit5);
         panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
-        //        panierDto.getMapPanier().put(produitDto6, ligneCommandeProduit6);
-        //        panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
-        //        panierDto.getMapPanier().put(produitDto7, ligneCommandeProduit7);
-        //        panierDto.setNombreDeReferences(1 + panierDto.getNombreDeReferences());
         return panierDto;
     }
 }
