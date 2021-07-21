438d828c2522c34299da4399f0684c6863d069f5
nathan.brichet <nathan.brichet@live.fr>
2021-07-21 12:18:33 +0200

    utilisation JUnit 5

--- src/test/java/service/panier/PanierServiceTest.java
+++ src/test/java/service/panier/PanierServiceTest.java
@@ -3,11 +3,11 @@
  */
 package service.panier;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 
