cfa75214abf0dc70ad2ecf59d620943aa5fb3c06
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-21 16:39:09 +0200

    hashage ok

--- src/main/java/service/util/impl/GenerateReferenceCommandeUtil.java
+++ src/main/java/service/util/impl/GenerateReferenceCommandeUtil.java
@@ -3,6 +3,9 @@
  */
 package service.util.impl;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -28,17 +31,72 @@ public class GenerateReferenceCommandeUtil extends AbstractGenerateReferenceUtil
 
     @Override
     public String generateReference(final Object... param) {
-        final var utilisateur = (UtilisateurDto) param[0];
-        final var nombreDeProduit = param[1].toString();
-        final var date = param[2].toString();
-        // FIXME : debug
-        System.out.println("Nom : " + utilisateur.getNom() + " Prénom : " + utilisateur.getPrenom() + " nombre de produits : "
-                + nombreDeProduit + " Date : " + date);
+        // on vérifie la cohérence des données reçues
+        if (isConform(param)) {
+            final var reference = hash(param);
+            logger.info("Référence {} générée par {}", reference, this.getClass().getSimpleName());
+            return reference;
+        }
+        return null;
+    }
 
-        // TODO : Hashage 
-        final var reference = "REFERENCETEST15";
-        logger.info("Référence {} générée par {}", reference, this.getClass().getSimpleName());
-        return reference;
+    private boolean isConform(final Object... param) {
+        // Pour être conforme, il faut :
+        // un UtilisateurDto
+        if (!(param[0] instanceof UtilisateurDto)) {
+            return false;
+        }
+        final var utilisateur = (UtilisateurDto) param[0];
+        // non null
+        if (null == utilisateur) {
+            return false;
+        }
+        // un Integer
+        if (!(param[1] instanceof Integer)) {
+            return false;
+        }
+        final var nombreDeProduit = (Integer) param[1];
+        // positif
+        if (nombreDeProduit < 0) {
+            return false;
+        }
+        // une date
+        if (!(param[2] instanceof Date)) {
+            return false;
+        }
+        final var date = (Date) param[2];
+        // non nulle
+        if (null == date) {
+            return false;
+        }
+        return true;
+    }
+
+    private String hash(final Object... param) {
+        final var utilisateur = (UtilisateurDto) param[0];
+        final var nombreDeProduit = (Integer) param[1];
+        // gestion de la date
+        final var date = (Date) param[2];
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        // le hash est composé de :
+        final StringBuilder hash = new StringBuilder();
+        // la première lettre du nom
+        hash.append(utilisateur.getNom().charAt(0));
+        // la première lettre du prénom
+        hash.append(utilisateur.getPrenom().charAt(0));
+        // le nombre de produit dans le panier
+        hash.append(String.format("%02d", nombreDeProduit));
+        // le jour du mois
+        hash.append(String.format("%02d", calendar.get(Calendar.DAY_OF_MONTH)));
+        // le mois. On ajoute un pour rester cohérent, pour Calendar Janvier = 0 décembre = 11 ! 
+        hash.append(String.format("%02d", calendar.get(Calendar.MONTH) + 1));
+        // l'année. On retire 2000 pour garder les deux chiffres significatifs de l'année.
+        hash.append(String.format("%02d", calendar.get(Calendar.YEAR) - 2000));
+        // la seconde de la journée. On fait les conversions élémentaires.
+        hash.append(String.format("%05d",
+                (calendar.get(Calendar.SECOND) + calendar.get(Calendar.MINUTE) * 60 + calendar.get(Calendar.HOUR) * 24 * 60)));
+        return hash.toString();
     }
 
 }
