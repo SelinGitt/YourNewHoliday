171d8003010e5e8565cc779e9b5ea09ca7d7e49d
Valentin J <vltn.justin@gmail.com>
2021-07-21 14:48:24 +0200

    Implementation du validator

--- src/main/java/presentation/utilisateur/controller/EditerUtilisateur.java
+++ src/main/java/presentation/utilisateur/controller/EditerUtilisateur.java
@@ -5,13 +5,16 @@ package presentation.utilisateur.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import presentation.utilisateur.dto.UtilisateurDto;
+import presentation.utilisateur.validator.EditerUtilisateurValidator;
 import service.utilisateur.IUtilisateurService;
 
 /**
@@ -24,7 +27,10 @@ import service.utilisateur.IUtilisateurService;
 public class EditerUtilisateur {
 
     @Autowired
-    private IUtilisateurService iUtilisateurService;
+    private IUtilisateurService        iUtilisateurService;
+
+    @Autowired
+    private EditerUtilisateurValidator editerUtilisateurValidator;
 
     /**
      * Permet de recuperer l'utilisateur et d'afficher la jsp
@@ -58,11 +64,32 @@ public class EditerUtilisateur {
      * 
      * @param  origin         String la page d'origine (1 pour USR00, 2 pour USR01)
      * @param  utilisateurDto l'utilisateur à mettre a jour
+     * @param  result         Resultats du binding utilisé pour gérer les erreurs
+     * @param  attribute      Permet de transmettre a travers la redirection
      * @return                redirection vers usr01 ou usr00 en fonction de la page d'origine
      */
     @PostMapping
-    public ModelAndView processSubmit(final UtilisateurDto utilisateurDto, @RequestParam("origin") final String origin) {
-        this.iUtilisateurService.updateUtilisateur(utilisateurDto);
+    public ModelAndView processSubmit(final UtilisateurDto utilisateurDto, @RequestParam("origin") final String origin,
+            final BindingResult result, final RedirectAttributes attribute) {
+        editerUtilisateurValidator.validate(utilisateurDto, result);
+
+        //Si le formulaire a des erreurs
+        if (result.hasErrors()) {
+            final var modelAndView = new ModelAndView("modifierUtilisateur");
+            modelAndView.getModelMap().addAttribute("error", "usr02.erreur.edit");
+            return modelAndView;
+        }
+
+        // Si utilisateur == null, l'email est deja pris
+        if (this.iUtilisateurService.updateUtilisateur(utilisateurDto) == null) {
+            result.rejectValue("email", "usr02.erreur.email_taken", "Default Errror");
+            final var modelAndView = new ModelAndView("modifierUtilisateur");
+            modelAndView.getModelMap().addAttribute("error", "usr02.erreur.edit");
+            return modelAndView;
+        }
+
+        attribute.addFlashAttribute("userSuccess", "usr02.success.update");
+
         if ("1".equals(origin)) {
             return new ModelAndView("redirect:/consulterUtilisateur.do");
         }
