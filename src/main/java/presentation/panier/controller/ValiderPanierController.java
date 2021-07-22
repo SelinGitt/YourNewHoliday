b35ffb246e73b54011217da57fd7d4dbdd2d2c12
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-22 15:31:30 +0200

    récupération des modif de Nathan

--- src/main/java/presentation/panier/controller/ValiderPanierController.java
+++ src/main/java/presentation/panier/controller/ValiderPanierController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 import presentation.commande.dto.AdressesDto;
 import presentation.panier.dto.PanierDto;
@@ -35,21 +36,44 @@ public class ValiderPanierController {
      * @param  adresses    liste des adresses récupérer du formulaire
      * @return             String l'url vers lequel on doit se rendre
      */
+    //    @PostMapping
+    //    public String passerPanierACommande(final @SessionAttribute("panierDto") PanierDto panierDto,
+    //            final @SessionAttribute("utilisateur") UtilisateurConnecteDto utilisateur,
+    //            final @ModelAttribute("adresses") AdressesDto adresses) {
+    //        final var referenceCommandeOuListProduitErreur = this.panierService.validerPanier(panierDto, adresses,
+    //                Integer.parseInt(utilisateur.getIdUtilisateur()));
+    //        if (referenceCommandeOuListProduitErreur == null) {
+    //            // On détruit la session donc le panier sera vider automatiquement (ici l'utilisateur a été supprimé et est null)
+    //            return "redirect:deconnecter.do";
+    //        }
+    //        if (referenceCommandeOuListProduitErreur.getReference() == null) {
+    //            // en cas d'erreur renvoie au panier
+    //            return "redirect:listerPanierProduits.do";
+    //        }
+    //        // renvoie à la page de détail des commandes
+    //        return "redirect:detailCommande.do?ref=" + referenceCommandeOuListProduitErreur.getReference();
+    //    }
+
     @PostMapping
-    public String passerPanierACommande(final @SessionAttribute("panierDto") PanierDto panierDto,
+    public ModelAndView passerPanierACommande(final @SessionAttribute("panierDto") PanierDto panierDto,
             final @SessionAttribute("utilisateur") UtilisateurConnecteDto utilisateur,
             final @ModelAttribute("adresses") AdressesDto adresses) {
+        final var modelAndView = new ModelAndView();
         final var referenceCommandeOuListProduitErreur = this.panierService.validerPanier(panierDto, adresses,
                 Integer.parseInt(utilisateur.getIdUtilisateur()));
         if (referenceCommandeOuListProduitErreur == null) {
             // On détruit la session donc le panier sera vider automatiquement (ici l'utilisateur a été supprimé et est null)
-            return "redirect:deconnecter.do";
+            modelAndView.setViewName("redirect:deconnecter.do");
+            return modelAndView;
         }
         if (referenceCommandeOuListProduitErreur.getReference() == null) {
             // en cas d'erreur renvoie au panier
-            return "redirect:listerPanierProduits.do";
+            modelAndView.setViewName("redirect:listerPanierProduits.do");
+            modelAndView.getModelMap().addAttribute("listIdError", referenceCommandeOuListProduitErreur.getListIdProduitNonConcordant());
+            return modelAndView;
         }
         // renvoie à la page de détail des commandes
-        return "redirect:detailCommande.do?ref=" + referenceCommandeOuListProduitErreur.getReference();
+        modelAndView.setViewName("redirect:detailCommande.do?ref=" + referenceCommandeOuListProduitErreur.getReference());
+        return modelAndView;
     }
 }
