ea3520c347390be176081086a320fa037535232c
bordeynea <bordeynea@gmail.com>
2021-07-22 16:53:04 +0200

    resolution pb

--- WebContent/js/footer/langues.js
+++ WebContent/js/footer/langues.js
@@ -7,11 +7,17 @@ const enButton = document.getElementById("EN-button");
 const lienFr = document.getElementById("lienLangueFr");
 const lienEn = document.getElementById("lienLangueEn");
 function selectLG() {
+
 	if (lang === "fr") {
 		frButton.className += " langue-button-clicked";
 		lienFr.className = "langue-pointer"
 	} else if (lang === "en") {
 		enButton.className += " langue-button-clicked";
 		lienEn.className = "langue-pointer"
+	}
+	//par default la langue est FR le curseur sera donc positioner sur fr a la connexion ou a la deconnexion
+	else {
+		frButton.className += " langue-button-clicked";
+		lienFr.className = "langue-pointer"
 	}
 }
