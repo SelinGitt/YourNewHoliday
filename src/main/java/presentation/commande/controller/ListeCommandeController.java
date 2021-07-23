eee22dc1e3c1b8eb72ba3a41894b8f874c35a615
Valentin J <vltn.justin@gmail.com>
2021-07-23 11:00:59 +0200

    Ajout de la possibilite de recup les commandes selon idUtilisateur

--- src/main/java/presentation/commande/controller/ListeCommandeController.java
+++ src/main/java/presentation/commande/controller/ListeCommandeController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import presentation.commande.dto.CommandeDto;
@@ -35,17 +36,29 @@ public class ListeCommandeController {
 
     /**
      * Permet d'accéder à la page listerCommande do et de l'hydrater
-     *
-     * @param  session contient la session de l'utilisateur
-     * @return         ModelAndView le modèle qui sera utiliser par la vue
+     * 
+     * @param  idUtilisateur ID de l'utilisateur si on souhaite recup les commandes via liste user admin
+     * @param  session       contient la session de l'utilisateur
+     * @return               ModelAndView le modèle qui sera utiliser par la vue
      */
     @GetMapping
-    public ModelAndView listerCommande(final HttpSession session) {
+    public ModelAndView listerCommande(final @RequestParam(name = "id", required = false, defaultValue = "") String idUtilisateur,
+            final HttpSession session) {
+        if (idUtilisateur.isEmpty()) {
+            final var modelAndView = new ModelAndView();
+            final UtilisateurConnecteDto utilisateurConnecte = (UtilisateurConnecteDto) session.getAttribute("utilisateur");
+            this.logger.debug("lister Commande utilisateur : {} ", utilisateurConnecte.getIdUtilisateur());
+            final List<CommandeDto> listCommande = this.iCommandeService
+                    .listerCommandesUtilisateur(Integer.valueOf(utilisateurConnecte.getIdUtilisateur()));
+            modelAndView.setViewName("listerCommande");
+            modelAndView.getModelMap().addAttribute("listCommande", listCommande);
+            return modelAndView;
+        }
+
+        this.logger.debug("lister Commande utilisateur : {} ", idUtilisateur);
+        final List<CommandeDto> listCommande = this.iCommandeService.listerCommandesUtilisateur(Integer.valueOf(idUtilisateur));
+
         final var modelAndView = new ModelAndView();
-        final UtilisateurConnecteDto utilisateurConnecte = (UtilisateurConnecteDto) session.getAttribute("utilisateur");
-        this.logger.debug("lister Commande utilisateur : {} ", utilisateurConnecte.getIdUtilisateur());
-        final List<CommandeDto> listCommande = this.iCommandeService
-                .listerCommandesUtilisateur(Integer.valueOf(utilisateurConnecte.getIdUtilisateur()));
         modelAndView.setViewName("listerCommande");
         modelAndView.getModelMap().addAttribute("listCommande", listCommande);
         return modelAndView;
