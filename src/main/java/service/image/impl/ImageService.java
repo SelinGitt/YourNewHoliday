60e8ccb1ae2c84612ca9cc2ea1fb627bc7116534
Lduval62 <87565719+Lduval62@users.noreply.github.com>
2021-07-28 11:58:27 +0200

    corrections commentaires gitlab

--- src/main/java/service/image/impl/ImageService.java
+++ src/main/java/service/image/impl/ImageService.java
@@ -3,6 +3,7 @@
  */
 package service.image.impl;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -31,7 +32,10 @@ import service.util.GetPropertyValues;
 @Transactional(propagation = Propagation.REQUIRED)
 public class ImageService implements IImageService {
 
-    private static final Logger logger = LoggerFactory.getLogger(ImageService.class);
+    private static final Logger logger            = LoggerFactory.getLogger(ImageService.class);
+    private static final int    LIMIT_WIDTH_USER  = 200;
+    private static final int    LIMIT_HEIGHT_USER = 200;
+    private static final int    LIMIT_SIZE_USER   = 500_000;
 
     @Autowired
     private IImageDao           imageDao;
@@ -65,7 +69,9 @@ public class ImageService implements IImageService {
         if (TypeImage.UTILISATEUR.getType().equals(type)) {
             final String cheminComplet = GetPropertyValues.getPropertiesMap().get("imagesUtilisateursRepo") + File.separator + fileName;
             //utilisatiion de l'écriture java 7 pour indiquer que le fichier ne doit pas dépasser 500ko
-            return imageDao.saveImage(cheminComplet, byteArray) && verifyFile(cheminComplet, 200, 200, 500_000);
+            final var imageEnregistree = imageDao.saveImage(cheminComplet, byteArray);
+            final var imageValid = verifyFile(cheminComplet, LIMIT_WIDTH_USER, LIMIT_HEIGHT_USER, LIMIT_SIZE_USER);
+            return imageEnregistree && imageValid;
         }
         logger.debug("Le type {} du fichier ne correspond pas à un type existant", type);
         return false;
@@ -85,12 +91,20 @@ public class ImageService implements IImageService {
         final var file = new File(chemin);
         try {
             final var bufferImage = ImageIO.read(file);
-            return !(bufferImage == null || bufferImage.getWidth() > width || bufferImage.getHeight() > height || file.length() > size);
+            return !(bufferImage == null || isImageValid(bufferImage, height, width) || isFileValid(file, size));
 
         } catch (final IOException ioe) {
             logger.error("une exception {} a été levée pour le fichier {}", ioe, file);
             return false;
         }
+    }
+
+    private boolean isImageValid(final BufferedImage bufferImage, final int height, final int width) {
+        return bufferImage.getWidth() > width || bufferImage.getHeight() > height;
+    }
+
+    private boolean isFileValid(final File file, final int size) {
+        return file.length() > size;
     }
 
 }
