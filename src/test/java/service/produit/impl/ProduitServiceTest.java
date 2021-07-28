d47af30d37adc0054743a6296ada4d253fcc63b1
damien.dugaz@gmail.com <damien.dugaz@gmail.com>
2021-07-28 10:07:06 +0200

    conflicts fixed

--- src/test/java/service/produit/impl/ProduitServiceTest.java
+++ src/test/java/service/produit/impl/ProduitServiceTest.java
@@ -3,8 +3,8 @@
  */
 package service.produit.impl;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
