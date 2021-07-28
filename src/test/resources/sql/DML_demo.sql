6fb48af35afd838b255f85601c92a9df0fc9402c
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-28 14:55:50 +0200

    retour à la normale

--- src/test/resources/sql/DML_demo.sql
+++ src/test/resources/sql/DML_demo.sql
@@ -74,6 +74,8 @@ INSERT INTO `droit` (`idDroit`, `url`) VALUES (23, 'validerPanierProduits.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (24, 'ajouterProduitPanier.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (25, 'validerPanier.do');
 INSERT INTO `droit` (`idDroit`, `url`) VALUES (26, 'supprimerProduitAdmin.do');
+INSERT INTO `droit` (`idDroit`, `url`) VALUES (27, 'consulterProduitAchete.do');
+
 
 
 --
@@ -212,3 +214,5 @@ INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (48, 24, 3);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (49, 25, 2);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (50, 25, 3);
 INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (51, 26, 3);
+INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (52, 27, 2);
+INSERT INTO `possede` (`idPossede`, `idDroit`, `idRole`) VALUES (53, 27, 3);
