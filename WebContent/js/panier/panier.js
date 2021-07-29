3866204712a0c561115fc7844d641fc9171cca2f
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-29 14:49:32 +0200

    déclaration du type bouton pour les boutons de reset de données

--- WebContent/js/panier/panier.js
+++ WebContent/js/panier/panier.js
@@ -5,12 +5,12 @@
 function informationsLivraison() {
 	document.getElementById("livraisonNom").value = document.getElementById("defaultNom").value;
 	document.getElementById("livraisonPrenom").value = document.getElementById("defaultPrenom").value;
-	document.getElementById("livraisonAdresse").value = document.getElementById("defaultAdresse").value;
+	document.getElementById("livraisonAdresse").value = document.getElementById("defaultAdresse").value;	
 }
 
 //reset le formulaire adresse de facturation
 function informationsFacturation() {
 	document.getElementById("facturationNom").value = document.getElementById("defaultNom").value;
 	document.getElementById("facturationPrenom").value = document.getElementById("defaultPrenom").value;
-	document.getElementById("facturationAdresse").value = document.getElementById("defaultAdresse").value;
+	document.getElementById("facturationAdresse").value = document.getElementById("defaultAdresse").value;	
 }
