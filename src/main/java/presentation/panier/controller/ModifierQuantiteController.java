87cd95eea544536ce67b6d462490c6041a38187d
SteveDebuchy <stevedebuchy@gmail.com>
2021-07-29 13:33:37 +0200

    test ok

--- src/main/java/presentation/panier/controller/ModifierQuantiteController.java
+++ src/main/java/presentation/panier/controller/ModifierQuantiteController.java
@@ -3,6 +3,9 @@
  */
 package presentation.panier.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import presentation.panier.dto.PanierDto;
+import presentation.produit.dto.ProduitDto;
 import service.panier.IPanierService;
+import service.produit.IProduitService;
 
 /**
  * Class represents controller pour les boutons modifiant la quantité d'une produit du panier
@@ -25,23 +31,45 @@ import service.panier.IPanierService;
 @RequestMapping("/modifierQuantite.do")
 public class ModifierQuantiteController {
     @Autowired
-    private IPanierService panierService;
+    private IPanierService  panierService;
+
+    @Autowired
+    private IProduitService produitService;
 
     /**
      * Permets de modifier la quantité d'une produit du panier
      *
      * @param  panierDto le panier
      * @param  request   envoyée
+     * @param redirectAttributes pour ajouter la liste d'erreur si besoin
      * @return           le model and view
      */
     @GetMapping
-    public ModelAndView modifierQuantite(final @SessionAttribute("panierDto") PanierDto panierDto, final HttpServletRequest request) {
+    public ModelAndView modifierQuantite(final @SessionAttribute("panierDto") PanierDto panierDto, final HttpServletRequest request,
+            final RedirectAttributes redirectAttributes) {
         final var modelAndView = new ModelAndView();
-        panierService.modifierQuantite(panierDto, Integer.valueOf(request.getParameter("idProduit")),
-                Integer.valueOf(request.getParameter("quantite")));
-        //renvoyer vers listerPanierProduitController
+        // quoiqu'il arrive on redirige vers le panier
         modelAndView.setViewName("redirect:listerPanierProduits.do");
+        final var id = Integer.valueOf(request.getParameter("idProduit"));
+        // test de conformité
+        if (!isConforme(id)) {
+            final List<Integer> listIdError = new ArrayList<>();
+            listIdError.add(id);
+            redirectAttributes.addFlashAttribute("listIdError", listIdError);
+            return modelAndView;
+        }
+        panierService.modifierQuantite(panierDto, id, Integer.valueOf(request.getParameter("quantite")));
         return modelAndView;
+
+    }
+
+    private boolean isConforme(final Integer id) {
+        final ProduitDto produitEnvente = produitService.trouverProduitEnVente(id);
+        if (produitEnvente == null) {
+            return false;
+        }
+        // TODO : controle de la version.
+        return true;
     }
 
 }
